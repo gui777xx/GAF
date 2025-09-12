76025d9b3c79df05806ffd578c2dbdb2c044c6a1
gui777x <guizin.clementino@gmail.com>
2025-09-12 11:09:17 -0300

    Criando  Repositório

--- GAF.API/Repositories/CategoryRepository.cs
+++ GAF.API/Repositories/CategoryRepository.cs
@@ -5,16 +5,7 @@ namespace GAF.API.Repositories;
 
 public class CategoryRepository : ICategoryRepository
 {
-    public Task<Category> CreateAsync(Category category)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<bool> DeleteAsync(int id, string userId)
-    {
-        throw new NotImplementedException();
-    }
-
+   
     public Task<IEnumerable<Category>> GetAllByUserIdAsync(string userId)
     {
         throw new NotImplementedException();
@@ -30,13 +21,20 @@ public class CategoryRepository : ICategoryRepository
         
         throw new NotImplementedException();
     }
-
-    public Task<bool> HasTransactionAsync(int id, string userId)
+     public Task<Category> CreateAsync(Category category)
     {
         throw new NotImplementedException();
     }
-
-    public Task<Category> UpdateAsync(Category category)
+        public Task<Category> UpdateAsync(Category category)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public Task<bool> DeleteAsync(int id, string userId)
+    {
+        throw new NotImplementedException();
+    }
+    public Task<bool> HasTransactionAsync(int id, string userId)
     {
         throw new NotImplementedException();
     }
